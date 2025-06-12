edfc2b5e4fece6a91fffa42caf3ce86f07f6ad3f
chelxten <151263716+chelxten@users.noreply.github.com>
2025-06-12 16:18:30 +0100

    Update 3_final_download.py

--- pages/3_final_download.py
+++ pages/3_final_download.py
@@ -44,7 +44,7 @@ def format_tour_plan_for_html(tour_plan):
     html = "<ul>"
     for l in route_lines:
         l = remove_emojis(l)
-        html += f"{l}"
+        html += f"<li>{l}</li>"
     html += "</ul>"
     return html
 
@@ -59,10 +59,10 @@ def generate_dynamic_pdf_html(name, signature, tour_plan, rating, feedback):
         body {{ font-family: Arial, sans-serif; margin: 40px; }}
         h1 {{ text-align: center; color: #990033; font-size: 14pt;}}
         h2 {{ color: #990033; border-bottom: 1px solid #ddd; padding-bottom: 4px; font-size: 14pt;}}
-        table {{ width: 100%; font-size: 12pt; border-collapse: collapse; margin-bottom: 20px; }}
+        table {{ width: 100%; font-size: 10pt; border-collapse: collapse; margin-bottom: 20px; }}
         td {{ padding: 6px; vertical-align: top; }}
-        ul {{ font-size: 10pt; }}
-        p {{ font-size: 10pt; }}
+        ul {{ font-size: 12pt; }}
+        p {{ font-size: 12pt; }}
     </style>
     </head>
     <body>
