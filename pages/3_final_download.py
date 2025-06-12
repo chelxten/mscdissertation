9d4af9b9bf7652a82cd8899f1ad9a3730b375a42
chelxten <151263716+chelxten@users.noreply.github.com>
2025-06-12 15:57:13 +0100

    Update 3_final_download.py

--- pages/3_final_download.py
+++ pages/3_final_download.py
@@ -22,12 +22,13 @@ unique_id = st.session_state.get("unique_id", "Unknown")
 def remove_emojis(text):
     emoji_pattern = re.compile(
         "["
-        "\U0001F600-\U0001F64F"
-        "\U0001F300-\U0001F5FF"
-        "\U0001F680-\U0001F6FF"
-        "\U0001F1E0-\U0001F1FF"
+        "\U0001F600-\U0001F64F"  # Emoticons
+        "\U0001F300-\U0001F5FF"  # Symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # Transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # Flags
         "\U00002702-\U000027B0"
         "\U000024C2-\U0001F251"
+        "\U000025A0-\U000025FF"  # Geometric Shapes (black squares, boxes)
         "]+",
         flags=re.UNICODE
     )
